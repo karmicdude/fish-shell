874c71330c5b12cda909adcaacdb16aba8658a2a
Voronov S.V <voronov.semyon@gmail.com>
2021-06-05 21:17:40 +0900

    Add adsf and zoxide initialization

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,14 @@
 # Maintainer: Voronov S.V <voronov.semyon@gmail.com>
 # Git Repos: https://github.com/karmicdude/fish-shell
 #            https://github.com/karmicdude/dotfiles
+
+fish_add_path $HOME/.local/bin
+
+if test -e /opt/asdf-vm/asdf.fish
+  source /opt/asdf-vm/asdf.fish
+  set -gx ASDF_DATA_DIR $HOME/.cache/adsf
+end
+
+if type -q zoxide
+  zoxide init fish | source
+end
