ffee79afd42636d8f7dde4b353ecee40b27530a6
Voronov S.V <voronov.semyon@gmail.com>
2021-06-06 03:26:06 +0900

    Add abbreviations

--- .config/fish/init.fish
+++ .config/fish/init.fish
@@ -1,12 +1,17 @@
 #!/usr/bin/env fish
 
 set -U fish_greeting
-
 set -Ux SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
-
 set -Ux EDITOR vim
 set -Ux VISUAL $EDITOR
 set -Ux PAGER "bat -nn"
 set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# Abbreviations
+abbr -a -U k kubectl
+abbr -a -U kns kubens
+abbr -a -U kx kubectx
+abbr -a -U tf terraform
+abbr -a -U mc mcli
 
 # vim: ft=fish
