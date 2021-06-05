f34e172e9f8396b4189b4e942530c1094a03f29f
Voronov S.V <voronov.semyon@gmail.com>
2021-06-06 03:25:49 +0900

    Fix indent fish fish_indent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-# Fish init file
 # Maintainer: Voronov S.V <voronov.semyon@gmail.com>
 # Git Repos: https://github.com/karmicdude/fish-shell
 #            https://github.com/karmicdude/dotfiles
@@ -6,14 +5,20 @@
 fish_add_path $HOME/.local/bin
 
 if test -e /opt/asdf-vm/asdf.fish
-  source /opt/asdf-vm/asdf.fish
-  set -gx ASDF_DATA_DIR $HOME/.cache/adsf
+    source /opt/asdf-vm/asdf.fish
+    set -gx ASDF_DATA_DIR $HOME/.cache/adsf
 end
 
 if type -q zoxide
-  zoxide init fish | source
+    zoxide init fish | source
 end
 
 if type -q direnv
-  direnv hook fish | source
+    direnv hook fish | source
+end
+
+if status --is-interactive
+    if test -f ~/.kube/config_list
+        set -gx KUBECONFIG (paste -sd ':' ~/.kube/config_list)
+    end
 end
