03cec7a4456d83c7b9e430b5456e3c804916157f
Voronov S.V <voronov.semyon@gmail.com>
2021-06-06 16:52:08 +0900

    Set variables with one action

--- .config/fish/functions/farm.fish
+++ .config/fish/functions/farm.fish
@@ -8,11 +8,18 @@
 function farm
     set -l config ~/.config/farm_connection.json
     set -l farm $argv[1]
-    set -l domain (jq -r '.domain' $config)
-    set -l username (jq -r '.username' $config)
-    if test -r $config
-        jq -r '.password' $config | anydesk "$farm.$domain/$username" --with-password &>/dev/null
+    if not test -r $config
+        set_color red
+        echo "No such config file $config" >&2
+        set_color normal
+        return 1
     end
+    jq -r '[.username, .domain, .password] | @tsv' $config | read -l username domain password
+    if not pidof anydesk &>/dev/null
+        anydesk --local-service &>/dev/null &
+        sleep 1
+    end
+    echo $password | anydesk "$farm.$domain/$username" --with-password &>/dev/null &
 end
 
 # vim: ft=fish
