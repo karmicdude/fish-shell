33fef5677027db1607d4f56e2ffe4ce08d43d8f8
Voronov S.V <voronov.semyon@gmail.com>
2021-06-07 14:51:43 +0900

    Add icon for git dirs

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,6 +1,6 @@
 function fish_title
   if set -l git_dir (git rev-parse --show-toplevel 2>/dev/null)
-      echo "G:"(basename $git_dir)
+      echo " "(basename $git_dir)
   else
       echo $STARSHIP_SHELL
   end
