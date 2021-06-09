9a12c1c02de7610280584945be0b3cd8c40ba36f
Voronov S.V <voronov.semyon@gmail.com>
2021-06-09 15:49:21 +0900

    Add remote titile support

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,9 +1,12 @@
 function fish_title
-  if set -l git_dir (git rev-parse --show-toplevel 2>/dev/null)
-      echo " "(basename $git_dir)
-  else
-      echo $STARSHIP_SHELL
-  end
+    if set -l git_dir (git rev-parse --show-toplevel 2>/dev/null)
+        echo " "(basename $git_dir)
+    else if type -q SSH_CLIENT
+        echo (hostname)
+    else
+        echo "fish"
+    end
+
 end
 
 # vim: ft=fish
